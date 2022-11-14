08fe445f4f290aeab48ac809b7b1481614e3d14c
RafaelMSuarez <rafael.martinez1601@alumnos.ubiobio.cl>
2022-11-13 23:09:37 -0300

    se comenta posibles funciones redundantes

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -38,21 +38,21 @@ const updatePost = (req, res) => {
   });
 };
 
-//reporta un post
-const reportPost = (req, res) => {
-  const { id } = req.params;
-  Post.findByIdAndUpdate(id, { $inc: { reports: 1 } }, (err, post) => {
-    if (err) {
-      return res
-        .status(400)
-        .send({ message: "No se pudo actualizar la publicación" });
-    }
-    if (!post) {
-      return res.status(404).send({ message: "No se encontro la publicación" });
-    }
-    return res.status(200).send({ message: "Publicación reportada" });
-  });
-};
+// //reporta un post
+// const reportPost = (req, res) => {
+//   const { id } = req.params;
+//   Post.findByIdAndUpdate(id, { $inc: { reports: 1 } }, (err, post) => {
+//     if (err) {
+//       return res
+//         .status(400)
+//         .send({ message: "No se pudo actualizar la publicación" });
+//     }
+//     if (!post) {
+//       return res.status(404).send({ message: "No se encontro la publicación" });
+//     }
+//     return res.status(200).send({ message: "Publicación reportada" });
+//   });
+// };
 
 //elimina un post
 const deletePost = (req, res) => {
@@ -134,118 +134,111 @@ const getReportedPosts = (req, res) => {
     });
 };
 
-//da like a un post
-const likePost = (req, res) => {
-  const { id } = req.params;
-  Post.findByIdAndUpdate(id, { $inc: { likes: 1 } }, (err, post) => {
-    if (err) {
-      return res
-        .status(400)
-        .send({ message: "No se pudo actualizar la publicación" });
-    }
-    if (!post) {
-      return res.status(404).send({ message: "No se encontro la publicación" });
-    }
-    return res.status(200).send({ message: "Like realizado" });
-  });
-};
+// //da like a un post
+// const likePost = (req, res) => {
+//   const { id } = req.params;
+//   Post.findByIdAndUpdate(id, { $inc: { likes: 1 } }, (err, post) => {
+//     if (err) {
+//       return res
+//         .status(400)
+//         .send({ message: "No se pudo actualizar la publicación" });
+//     }
+//     if (!post) {
+//       return res.status(404).send({ message: "No se encontro la publicación" });
+//     }
+//     return res.status(200).send({ message: "Like realizado" });
+//   });
+// };
 
-//quita like a un post
-const likeMenosPost = (req, res) => {
-  const { id } = req.params;
-  Post.findByIdAndUpdate(id, { $inc: { likes: -1 } }, (err, post) => {
-    if (err) {
-      return res
-        .status(400)
-        .send({ message: "No se pudo actualizar la publicación" });
-    }
-    if (!post) {
-      return res.status(404).send({ message: "No se encontro la publicación" });
-    }
-    return res.status(200).send({ message: "Like quitado" });
-  });
-};
+// //quita like a un post
+// const likeMenosPost = (req, res) => {
+//   const { id } = req.params;
+//   Post.findByIdAndUpdate(id, { $inc: { likes: -1 } }, (err, post) => {
+//     if (err) {
+//       return res
+//         .status(400)
+//         .send({ message: "No se pudo actualizar la publicación" });
+//     }
+//     if (!post) {
+//       return res.status(404).send({ message: "No se encontro la publicación" });
+//     }
+//     return res.status(200).send({ message: "Like quitado" });
+//   });
+// };
 
-//da dislike a un post
-const dislikePost = (req, res) => {
-  const { id } = req.params;
-  Post.findByIdAndUpdate(id, { $inc: { dislikes: 1 } }, (err, post) => {
-    if (err) {
-      return res
-        .status(400)
-        .send({ message: "No se pudo actualizar la publicación" });
-    }
-    if (!post) {
-      return res.status(404).send({ message: "No se encontro la publicación" });
-    }
-    return res.status(200).send({ message: "Like realizado" });
-  });
-};
+// //da dislike a un post
+// const dislikePost = (req, res) => {
+//   const { id } = req.params;
+//   Post.findByIdAndUpdate(id, { $inc: { dislikes: 1 } }, (err, post) => {
+//     if (err) {
+//       return res
+//         .status(400)
+//         .send({ message: "No se pudo actualizar la publicación" });
+//     }
+//     if (!post) {
+//       return res.status(404).send({ message: "No se encontro la publicación" });
+//     }
+//     return res.status(200).send({ message: "Like realizado" });
+//   });
+// };
 
-//quita dislike a un post
-const dislikeMenosPost = (req, res) => {
-  const { id } = req.params;
-  Post.findByIdAndUpdate(id, { $inc: { dislikes: -1 } }, (err, post) => {
-    if (err) {
-      return res
-        .status(400)
-        .send({ message: "No se pudo actualizar la publicación" });
-    }
-    if (!post) {
-      return res.status(404).send({ message: "No se encontro la publicación" });
-    }
-    return res.status(200).send({ message: "Like realizado" });
-  });
-};
+// //quita dislike a un post
+// const dislikeMenosPost = (req, res) => {
+//   const { id } = req.params;
+//   Post.findByIdAndUpdate(id, { $inc: { dislikes: -1 } }, (err, post) => {
+//     if (err) {
+//       return res
+//         .status(400)
+//         .send({ message: "No se pudo actualizar la publicación" });
+//     }
+//     if (!post) {
+//       return res.status(404).send({ message: "No se encontro la publicación" });
+//     }
+//     return res.status(200).send({ message: "Like realizado" });
+//   });
+// };
 
-// se realiza un comentario
-const newComment = (req, res) => {
-  const { id } = req.params;
-  Post.findByIdAndUpdate(id, { $inc: { numComments: 1 } }, (err, post) => {
-    if (err) {
-      return res
-        .status(400)
-        .send({ message: "No se pudo actualizar la publicación" });
-    }
-    if (!post) {
-      return res.status(404).send({ message: "No se encontro la publicación" });
-    }
-    return res
-      .status(200)
-      .send({ message: "Numero de comentarios actualizado" });
-  });
-};
+// // se realiza un comentario
+// const newComment = (req, res) => {
+//   const { id } = req.params;
+//   Post.findByIdAndUpdate(id, { $inc: { numComments: 1 } }, (err, post) => {
+//     if (err) {
+//       return res
+//         .status(400)
+//         .send({ message: "No se pudo actualizar la publicación" });
+//     }
+//     if (!post) {
+//       return res.status(404).send({ message: "No se encontro la publicación" });
+//     }
+//     return res
+//       .status(200)
+//       .send({ message: "Numero de comentarios actualizado" });
+//   });
+// };
 
-// se realiza un comentario
-const commentDeleted = (req, res) => {
-  const { id } = req.params;
-  Post.findByIdAndUpdate(id, { $inc: { numComments: -1 } }, (err, post) => {
-    if (err) {
-      return res
-        .status(400)
-        .send({ message: "No se pudo actualizar la publicación" });
-    }
-    if (!post) {
-      return res.status(404).send({ message: "No se encontro la publicación" });
-    }
-    return res
-      .status(200)
-      .send({ message: "Numero de comentarios actualizado" });
-  });
-};
+// // se realiza un comentario
+// const commentDeleted = (req, res) => {
+//   const { id } = req.params;
+//   Post.findByIdAndUpdate(id, { $inc: { numComments: -1 } }, (err, post) => {
+//     if (err) {
+//       return res
+//         .status(400)
+//         .send({ message: "No se pudo actualizar la publicación" });
+//     }
+//     if (!post) {
+//       return res.status(404).send({ message: "No se encontro la publicación" });
+//     }
+//     return res
+//       .status(200)
+//       .send({ message: "Numero de comentarios actualizado" });
+//   });
+// };
 
 module.exports = {
   createPost,
   updatePost,
-  reportPost,
   deletePost,
   getPost,
   getPosts,
   getReportedPosts,
-  likePost,
-  likeMenosPost,
-  dislikePost,
-  dislikeMenosPost,
-  newComment,
-  commentDeleted,
 };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -6,14 +6,7 @@ const postController = require("../controllers/postController");
 api.post("/post", postController.createPost);
 
 //put
-api.put("/post/likepost/:id", postController.likePost);
-api.put("/post/likemenospost/:id", postController.likeMenosPost);
-api.put("/post/dislike/:id", postController.dislikePost);
-api.put("/post/dislikemenospost/:id", postController.dislikeMenosPost);
-api.put("/post/updatenumcomment/:id", postController.newComment)
-api.put("/post/updatenumcommentdeleted/:id", postController.commentDeleted)
 api.put("/post/update/:id", postController.updatePost);
-api.put("/post/report/:id", postController.reportPost);
 
 //delete
 api.delete("/post/delete/:id", postController.deletePost);
