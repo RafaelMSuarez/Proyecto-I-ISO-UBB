961885120dbade2d1fa9994a26b7a296c0314ea5
RafaelMSuarez <rafael.martinez1601@alumnos.ubiobio.cl>
2022-11-27 13:21:18 -0300

    cambios controladores

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -5,7 +5,7 @@ const fileSize = require("../middlewares/fileSize")
 
 const api = express.Router()
 
-// api.post("/file/:archivo",upload.array('archivos').fileSize,fileController.uploadNewFile)
+api.post("/file/:archivo",upload.array('archivos').fileSize,fileController.uploadNewFile)
 api.get('/files',fileController.getFiles)
 api.get('/files/download/:id', fileController.getSpecificFile)
 
