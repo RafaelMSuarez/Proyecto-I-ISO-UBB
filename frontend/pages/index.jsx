9cbff2c6821ea20d3e23a2d28a9e98d62035a478
FelipeBurgos <felipe.burgos1901@alumnos.ubiobio.cl>
2022-12-18 19:19:49 -0300

    se agregan funciones

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,5 +1,20 @@
-import HomePage from "./HomePage";
+/*import HomePage from "./HomePage";
 
 export default function Home() {
   return HomePage();
 }
+*/
+
+import React from 'react';
+import LoginForm from './login-form';
+
+function App() {
+  return (
+    <div>
+      <h1>Inicio de sesión</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App; 
